4fffe7377ebe968c39b6a36dc71d9f039dda9824
pingagy <pingagy@126.com>
2017-10-16 22:00:34 +0800

    测试 Change-Id: I3ffa64c3b15a392ab431581eec10cf7fed19509b Signed-off-by: pingagy <pingagy@126.com>

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,9 +1,21 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import redis.clients.jedis.JedisPool;
 
 public class Test {
 
+	private JedisPool pool;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Test test = new Test();
+		test.initPool();
 
 	}
 
+	public void initPool(){
+		Locale locale1 = new Locale("zh", "CN"); 
+		final ResourceBundle bundle = ResourceBundle.getBundle("myconf.properties",locale1);
+		System.out.println(bundle.getString("redis.pool.maxActive"));
+	}
 }
