03d6bfac5965c33a013815a8e2e79eb2450a34a2
pingagy <pingagy@126.com>
2018-09-08 21:16:59 +0800

    [#00]test Change-Id: I2ecb211c2f06b95c956334f0aeaf3e67ccf133cd Signed-off-by: pingagy <pingagy@126.com>

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,8 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello git 2");
+		System.out.println("hello git 3");
+		System.out.println("aaaaaaa");
 	}
 
 }
